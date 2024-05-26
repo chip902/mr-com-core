afac2515176abf515531cc1480317b4c16ca6224
chip902 <68761150+chip902@users.noreply.github.com>
2024-05-26 00:01:25 -0400

    moving GTM to the head and streamlining declaritive tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
+import { GoogleTagManager } from "@next/third-parties/google";
 import ClientLayout from "./ClientLayout";
 import Footer from "./Footer";
 import Navbar from "./NavBar";
 import ServerLayout from "./server-layout";
-import { GoogleTagManager } from "@next/third-parties/google";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<ServerLayout>
 			<ClientLayout>
+				<GoogleTagManager gtmId="GTM-NCGDKWPN" />
 				<nav>
 					<Navbar />
 				</nav>
 				{children}
-				<GoogleTagManager gtmId="GTM-NCGDKWPN" />
 				<footer>
 					<Footer />
 				</footer>
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,11 @@
 import Hero from "./Hero";
-import NavBar from "./NavBar";
 import ProfileContainer from "./ProfileContainer";
 
 export default function Home() {
 	return (
 		<>
-			<nav>
-				<NavBar />
-			</nav>
-			<main>
-				<Hero header="Marilyn Rifkin, LCSW" subHeader="New York | Connecticut | Florida" />
-				<ProfileContainer />
-			</main>
+			<Hero header="Marilyn Rifkin, LCSW" subHeader="New York | Connecticut | Florida" />
+			<ProfileContainer />
 		</>
 	);
 }
