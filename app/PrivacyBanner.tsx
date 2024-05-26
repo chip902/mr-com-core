7e324c7e5f9040c54684088ad9763813e47466ff
chip902 <68761150+chip902@users.noreply.github.com>
2024-05-26 00:43:17 -0400

    Fixing privacy banner

--- app/PrivacyBanner.tsx
+++ app/PrivacyBanner.tsx
@@ -1,12 +1,19 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Box, Button, Slide, Text, useDisclosure } from "@chakra-ui/react";
 import NextLink from "next/link";
+
 const PrivacyBanner = () => {
 	const { isOpen, onToggle } = useDisclosure();
+	const [isCookieChecked, setIsCookieChecked] = useState(false);
 
 	useEffect(() => {
-		if (!document.cookie.includes("_cookiesAccepted")) onToggle();
-	}, [onToggle]);
+		if (!isCookieChecked) {
+			if (!document.cookie.includes("_cookiesAccepted")) {
+				onToggle();
+			}
+			setIsCookieChecked(true);
+		}
+	}, [isCookieChecked, onToggle]);
 
 	const handleAcceptCookies = () => {
 		const expires = new Date();
@@ -16,21 +23,19 @@ const PrivacyBanner = () => {
 	};
 
 	return (
-		<>
-			<Slide direction="bottom" in={isOpen} style={{ zIndex: 10 }}>
-				<Box flexDirection="row" p="40px" color="white" mt="4" bg="brand.800" rounded="md" shadow="md">
-					<Text mb={5}>
-						This website uses cookies to improve your experience. We will assume you accept this policy as long as you are using this website
-					</Text>
-					<Button mx={10} onClick={handleAcceptCookies}>
-						Accept Cookies
-					</Button>
-					<Button as={NextLink} href="/privacy-policy">
-						Privacy Policy
-					</Button>
-				</Box>
-			</Slide>
-		</>
+		<Slide direction="bottom" in={isOpen} style={{ zIndex: 10 }}>
+			<Box flexDirection="row" p="40px" color="white" mt="4" bg="brand.800" rounded="md" shadow="md">
+				<Text mb={5}>
+					This website uses cookies to improve your experience. We will assume you accept this policy as long as you are using this website.
+				</Text>
+				<Button mx={10} onClick={handleAcceptCookies}>
+					Accept Cookies
+				</Button>
+				<Button as={NextLink} href="/privacy-policy">
+					Privacy Policy
+				</Button>
+			</Box>
+		</Slide>
 	);
 };
 
